9db3574d085812f87785ff92deb8112a5998e6d5
Robert Pirs <robertpirs9@gmail.com>
2025-04-14 02:37:02 +0300

    added dependencies, connects correctly to my database, did not get docker to work with hot reload, left docker files in to containerize app when ready

--- src/main/java/org/example/login/LoginController.java
+++ src/main/java/org/example/login/LoginController.java
@@ -1,4 +1,18 @@
 package org.example.login;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
 public class LoginController {
+
+    @GetMapping
+    public String login(){
+        return "login page lol12345566";
+    }
+
 }
